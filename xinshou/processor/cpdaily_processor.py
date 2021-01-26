6bfd959208e0967fc71a391cb9e1035fe7cf60fb
Licsber <licsber@gmail.com>
2021-01-26 22:08:43 +0800

    format: induce user to click ads.

--- xinshou/processor/cpdaily_processor.py
+++ xinshou/processor/cpdaily_processor.py
@@ -11,7 +11,8 @@ class CpDailyProcessor(Processor):
         exist = current_app.cpdaily.check_user_added(m.from_user_name)
         if not exist:
             token = current_app.cpdaily.gen_token(m.from_user_name)
-            content = f'<a href=\"https://wx.licsber.site/cpdaily/?token={token}\">完成认证后点我进入</a>'
+            content = f'<a href=\"https://wx.licsber.site/cpdaily/?token={token}\">' \
+                      f'进公众号文章点下底部广告后戳我进入</a>'
         else:
             content = '此微信已成功绑定1个账号, 如需更多请找舍友或联系管理员.'
         res = wx.reply.TextMsg(to_user, from_user, content)
